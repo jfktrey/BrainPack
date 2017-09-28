559a3b117debdaea0ff825a7b963918f0096867f
Trey Keown <jfktrey@gmail.com>
2017-09-27 20:22:24 -0700

    Package into a jar file

--- src/io/keown/brainpack/BrainPack.java
+++ src/io/keown/brainpack/BrainPack.java
@@ -4,36 +4,107 @@ import io.keown.brainpack.types.CharacterEncodingMap;
 import io.keown.brainpack.types.PackingBufferTooSmallException;
 import io.keown.brainpack.types.SizedBitSet;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.nio.file.Files.readAllBytes;
+import static java.nio.file.Paths.get;
 
 public class BrainPack {
     public static void main(String[] args) {
-        final String source = "<h+e-l.l,[]>! \naaa.";
+        if (args.length != 4) {
+            System.out.println("Four arguments are required.");
+            System.out.println("Usage: java -jar brainpack.jar source mapping filesize output");
+            System.out.println("    source   - Brainfuck file to be parsed");
+            System.out.println("    mapping  - Mapping file from Brainfuck instruction to encoded form");
+            System.out.println("    filesize - Size of generated file");
+            System.out.println("    output   - Output filename");
+            System.exit(1);
+        }
 
-        final Map<Character, SizedBitSet> encoding = new HashMap<>();
-        encoding.put('.', new SizedBitSet(0b0000, 4));
-        encoding.put(',', new SizedBitSet(0b0001, 4));
-        encoding.put('+', new SizedBitSet(0b0010, 4));
-        encoding.put('-', new SizedBitSet(0b0011, 4));
-        encoding.put('<', new SizedBitSet(0b0100, 4));
-        encoding.put('>', new SizedBitSet(0b0101, 4));
-        encoding.put('[', new SizedBitSet(0b0110, 4));
-        encoding.put(']', new SizedBitSet(0b0111, 4));
-
-        Packer packer = new Packer(source, new CharacterEncodingMap(encoding));
-        byte[] output = new byte[]{};
+        String source = null;
         try {
-            output = packer.pack(1024, ',');
-        } catch (PackingBufferTooSmallException e) {}
-
-        try {
-            Files.write(Paths.get("/Users/treykeown/test.bin"), output);
+            source = new String(readAllBytes(get(args[0])));
         } catch (IOException e) {
+            System.out.println("Unable to read source file.");
+            System.exit(1);
+        }
 
+        Map<Character, SizedBitSet> encodingMap = new HashMap<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(args[1]));
+            String line = reader.readLine();
+            Integer bitWidth = null;
+
+            try {
+                bitWidth = Integer.valueOf(line);
+            } catch (NumberFormatException e) {
+                System.out.println("Mapping file incorrect format. Error parsing bit width.");
+                System.exit(1);
+            }
+
+            while ((line = reader.readLine()) != null) {
+                List<String> lineParts = Arrays.stream(line.split("[ \t]*"))
+                        .filter(part -> !part.isEmpty())
+                        .collect(Collectors.toList());
+
+                if (lineParts.size() != 2) {
+                    System.out.println("Mapping file incorrect format. Too many parts per line.");
+                    System.exit(1);
+                }
+
+                if (lineParts.get(0).length() != 1) {
+                    System.out.println("Mapping file incorrect format. Too many characters in first column.");
+                    System.exit(1);
+                }
+
+                Character mapKey = lineParts.get(0).charAt(0);
+                Long mapValue = null;
+                try {
+                    mapValue = Long.valueOf(lineParts.get(1));
+                } catch (NumberFormatException e) {
+                    System.out.println("Mapping file incorrect format. Unable to parse encoding.");
+                    System.exit(1);
+                }
+
+                encodingMap.put(mapKey, new SizedBitSet(mapValue, bitWidth));
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("Unable to read mapping file.");
+            System.exit(1);
+        }
+
+        Integer fileSize = null;
+        try {
+            fileSize = Integer.valueOf(args[2]);
+        } catch (NumberFormatException e) {
+            System.out.println("File size must be an integer.");
+            System.exit(1);
+        }
+
+        Packer packer = new Packer(source, new CharacterEncodingMap(encodingMap));
+        byte[] output = null;
+        try {
+            output = packer.pack(fileSize, ',');
+        } catch (PackingBufferTooSmallException e) {
+            System.out.println("File size is too small.");
+            System.exit(1);
+        }
+
+        try {
+            Files.write(new File(args[3]).toPath(), output);
+        } catch (IOException e) {
+            System.out.println("Unable to open output file for writing.");
+            System.exit(1);
         }
     }
 }
